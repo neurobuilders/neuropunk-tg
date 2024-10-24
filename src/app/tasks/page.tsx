b6cb4c4c195c2be11df7d617a6befaf3805f0221
Konstantin Chernyshkov <kcrebound+eth@gmail.com>
2024-10-24 23:57:48 +0700

    added monstr

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -8,17 +8,24 @@ import { LocaleSwitcher } from "@/components/LocaleSwitcher/LocaleSwitcher";
 import { Page } from "@/components/Page";
 
 import tonSvg from "@/app/_assets/ton.svg";
+import {DotLottieReact} from "@lottiefiles/dotlottie-react";
+import React from "react";
+import Rive from "@rive-app/react-canvas";
 
 export default function Tasks() {
   const t = useTranslations("i18n");
 
   return (
     <Page back={false}>
-      <List>
-
-
-
-      </List>
+        <Rive
+            style={{
+                minHeight: '900px',
+                marginBottom: '6px',
+                marginTop: '3px',
+            }}
+            src="/cute_monster.riv"
+            stateMachines="IDLE"
+        />
     </Page>
   );
 }
