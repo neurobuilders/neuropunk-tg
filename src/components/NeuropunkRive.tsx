81d19243cdbf1895f3575e489c14628a3788bf2f
Konstantin Chernyshkov <kcrebound+eth@gmail.com>
2024-10-24 22:15:06 +0700

    fixes for layout

--- src/components/NeuropunkRive.tsx
+++ src/components/NeuropunkRive.tsx
@@ -4,18 +4,9 @@ export const NeuropunkRive = () => (
     <Rive
         style={{
             minHeight: '95px',
-            height: '100%',
+            height: '30%',
             marginBottom: '6px',
             marginTop: '3px',
-            '@media(min-width: 768px)': {
-                marginTop: '3px',
-                marginBottom: '8px'
-            },
-            '@media(min-width: 1024px)': {
-                marginBottom: '0',
-                marginTop: '0',
-                height: '150px'
-            }
         }}
         src="/neuropunk.riv"
     />
