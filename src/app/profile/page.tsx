9f62455b5b98c8609f59199e57dde3a2233be987
Konstantin Chernyshkov <kcrebound+eth@gmail.com>
2024-10-25 20:56:44 +0700

    added user profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,7 +13,7 @@ export default function Profile() {
       <Page back={true}>
         <List>
           <Section header="Profile">
-            <Cell before={<Image size={60} src="/neo.jpg" />} subtitle="Wake up">Neo Morheus</Cell>
+            <Cell before={<Image size="60" src="/neo.jpg" />} subtitle="Wake up">Neo Morheus</Cell>
             {/*<Cell subtitle="User's unique identifier">NeuroID: 47A3-M0RPH</Cell>*/}
             <Cell subtitle="Current level of the user">Level: 12</Cell>
             <Cell subtitle="Access information if available">NeuroPass: Active</Cell>
@@ -31,13 +31,13 @@ export default function Profile() {
           </Section>
 
           <Section header="Missions and Tasks">
-            <Cell>Complete Today's Mission: Secure the latest Neurospice trade route in Neurospace (Reward: 500 Neurospice).</Cell>
+            <Cell>Complete Today&#39;s Mission: Secure the latest Neurospice trade route in Neurospace (Reward: 500 Neurospice).</Cell>
             <Cell>Join NeuroSquad Mission: Collaborate with other captains to defend the Neurobunker (Reward: 1,000 Neurospice).</Cell>
             <Cell>Special Quest: Unlock new territories for the upcoming NeuroMarket expansion (Reward: Exclusive Neurospice boost and rank points).</Cell>
           </Section>
 
           <Section header="General Settings">
-            <Cell>Customize your profile to make Neurospace truly yours. Adjust privacy, appearance, and update your Captain's log to keep the Multiverse updated on your journey.</Cell>
+            <Cell>Customize your profile to make Neurospace truly yours. Adjust privacy, appearance, and update your Captain&#39;s log to keep the Multiverse updated on your journey.</Cell>
           </Section>
         </List>
       </Page>
