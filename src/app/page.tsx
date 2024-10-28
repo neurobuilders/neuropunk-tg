419221bfc3fa7e5d7be7b845e16a084cf0d36eef
Konstantin Chernyshkov <kcrebound+eth@gmail.com>
2024-10-28 12:02:53 +0700

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Avatar, Badge, Button, Card, Cell, List, Placeholder, Section } from "@telegram-apps/telegram-ui";
+import {Avatar, Badge, Button, Card, Cell, Chip, List, Placeholder, Section} from "@telegram-apps/telegram-ui";
 import { useTranslations } from "next-intl";
 
 import { Page } from "@/components/Page";
@@ -11,6 +11,7 @@ import React from "react";
 import { CardChip } from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardChip/CardChip";
 import { CardCell } from "@telegram-apps/telegram-ui/dist/components/Blocks/Card/components/CardCell/CardCell";
 import WelcomeToNeuroSpace from "@/components/WelcomeToNeuroSpace";
+import {IconCheckbox} from "@telegram-apps/telegram-ui/dist/components/Form/Checkbox/icons/checkbox";
 
 export default function Home() {
     const t = useTranslations("i18n");
@@ -18,84 +19,82 @@ export default function Home() {
     return (
         <Page back={false}>
             <List>
-                <Section header="Campaigns">
-                    <div className={"_padding-16"}>
+                <Section header="Releases">
+                    <div className={"padding-16"}>
                         <Card type="ambient">
                             <React.Fragment key=".0">
+                                {/* Display release rate */}
                                 <CardChip readOnly>
-                                    Featured Campaign
+                                    <span>+2.2k $NeuroPulse</span>
                                 </CardChip>
-                                <img
-                                    alt="Flappy Campaign"
-                                    src="https://cdn.neurocdn.ru/covers/flappy_campaign.jpg" // Replace with actual image link if available
-                                    style={{
-                                        display: 'block',
-                                        height: 308,
-                                        objectFit: 'cover',
-                                        width: 254
-                                    }}
-                                />
+
+                                <div>
+                                    <img
+                                        alt="Neuropunks LP 2"
+                                        src="https://cdn.neurocdn.ru/covers/NRPNK063.jpg"
+                                        style={{
+                                            display: 'block',
+                                            height: 308,
+                                            objectFit: 'cover',
+                                            width: 254
+                                        }}
+                                    />
+                                </div>
+
                                 <CardCell
                                     readOnly
-                                    subtitle="Flap and Earn! 3M $NOT among TOP-500"
+                                    subtitle="Various Artists"
+                                    after={<IconCheckbox/>}
                                 >
-                                    Flappy Campaign
+                                    Neuropunks LP 2
                                 </CardCell>
+
                             </React.Fragment>
                         </Card>
                     </div>
                 </Section>
 
-                <Section header="Releases">
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Hot Release
-                            </CardChip>
-                            <img
-                                alt="Neuropunks LP 2"
-                                src="https://cdn.neurocdn.ru/covers/NRPNK063.jpg"
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="Various Artists"
-                            >
-                                Neuropunks LP 2
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
-                </Section>
-
                 <Section header="Events">
-                    <Card type="ambient">
-                        <React.Fragment key=".0">
-                            <CardChip readOnly>
-                                Upcoming Event
-                            </CardChip>
-                            <img
-                                alt="Neuropunks Event"
-                                src="https://cdn.neurocdn.ru/covers/event_cover.jpg" // Replace with actual event image link if available
-                                style={{
-                                    display: 'block',
-                                    height: 308,
-                                    objectFit: 'cover',
-                                    width: 254
-                                }}
-                            />
-                            <CardCell
-                                readOnly
-                                subtitle="Live Event in Neuro Space"
-                            >
-                                Neuropunks Gathering
-                            </CardCell>
-                        </React.Fragment>
-                    </Card>
+                    <div className={"padding-16"}>
+                        <Card>
+                            <React.Fragment key=".0">
+                                <CardChip readOnly>
+                                    <div>
+                                        <div><span>+200k $NeuroPulse</span></div>
+                                        <div><span>200+ Neuropunkers</span></div>
+                                    </div>
+                                </CardChip>
+                                <div style={{}}>
+                                    <img
+                                        alt="Neuropunks Moscow | 21.02.2025"
+                                        src="https://neuropunk.ru/wp-content/uploads/2024/10/123123-1.jpg"
+                                        style={{
+                                            display: 'block',
+                                            height: 300,
+                                            objectFit: 'cover',
+                                            width: 520
+                                        }}
+                                    />
+                                </div>
+                                <CardCell
+                                    readOnly
+                                    subtitle="Atmosphere"
+                                >
+                                    Neuropunks Moscow | 21.02.2025
+                                </CardCell>
+                            </React.Fragment>
+                        </Card>
+                    </div>
+                </Section>
+                <Section header={"NeuroPulse Meter"}>
+                    <div className="padding-16">
+                        <img src="/neuropulse.png" style={{
+                            display: 'block',
+                            height: 800,
+                            objectFit: 'cover',
+                            width: 520
+                        }}/>
+                    </div>
                 </Section>
             </List>
         </Page>
