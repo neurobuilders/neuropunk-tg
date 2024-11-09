027ed50b4eebb8f088ec8e08f508c8456c5a9609
4matic <root@4matikku.com>
2024-11-10 01:16:02 +0200

    update NeuropunkRive component

--- src/components/NeuropunkRive.tsx
+++ src/components/NeuropunkRive.tsx
@@ -2,18 +2,25 @@
 
 // @ts-ignore
 import riveWASMResource from "@rive-app/canvas/rive.wasm";
-import Rive, { RuntimeLoader } from "@rive-app/react-canvas";
+import { useRive, RuntimeLoader } from "@rive-app/react-canvas";
 
 RuntimeLoader.setWasmUrl(riveWASMResource);
 
-export const NeuropunkRive = () => (
-  <Rive
-    style={{
-      minHeight: "95px",
-      height: "30%",
-      marginBottom: "6px",
-      marginTop: "3px",
-    }}
-    src="/neuropunk.riv"
-  />
-);
+export const NeuropunkRive = () => {
+  const { RiveComponent } = useRive({
+    src: "/neuropunk.riv",
+    // stateMachines: "bumpy",
+    useOffscreenRenderer: true,
+    autoplay: true,
+  });
+  return (
+    <RiveComponent
+      style={{
+        minHeight: "95px",
+        height: "30%",
+        marginBottom: "6px",
+        marginTop: "3px",
+      }}
+    />
+  );
+};
